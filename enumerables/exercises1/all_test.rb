a4c7b4a8d2093217eab2a63e06e7221c01040e20
DANIYAL AREEB <daniyalareeb123@gmail.com>
2020-10-13 13:04:10 +0530

    map upase test passed

--- enumerables/exercises1/all_test.rb
+++ enumerables/exercises1/all_test.rb
@@ -57,9 +57,11 @@ class AllTest < Minitest::Test
   end
 
   def test_not_all_uppercase
-    skip
     words = ["DOUGHNUT", "CASH", "MAIN", "bOWl", "SMACK", "SAND"]
-    # Your code goes here
+    all_caps = true
+    words.map do |word|
+      all_caps = false unless word == word.upcase
+    end
     refute all_caps
   end
 
