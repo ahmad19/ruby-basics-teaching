dfa51cbac2b8ae161580f8eb0b10608d1901ec09
DANIYAL AREEB <daniyalareeb123@gmail.com>
2020-11-03 11:54:09 +0530

    slect ending test passed

--- enumerables/exercises1/select_pattern_test.rb
+++ enumerables/exercises1/select_pattern_test.rb
@@ -37,9 +37,8 @@ class SelectPatternTest < Minitest::Test
   end
 
   def test_pick_words_ending_in_e
-    skip
     words = ["are", "you", "strike", "thinking", "belt", "piece", "warble", "sing", "pipe"]
-    # Your code goes here
+    selected = words.select! {|word| word.end_with? "e"}
     assert_equal ["are", "strike", "piece", "warble", "pipe"], selected
   end
 
